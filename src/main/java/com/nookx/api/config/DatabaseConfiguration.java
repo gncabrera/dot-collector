cbb0b5b2dd4022bc506ce34535571fd2c4f870d9
Gustavo Cabrera <gcabrera.rex@gmail.com>
2026-04-24 15:31:03 -0300

    Revert "fetaure: initial scraper version"
    This reverts commit 09c078b41b7481a23d45d68731a92c21b7dd1bf5.

--- src/main/java/com/nookx/api/config/DatabaseConfiguration.java
+++ src/main/java/com/nookx/api/config/DatabaseConfiguration.java
@@ -6,7 +6,7 @@ import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 @Configuration
-@EnableJpaRepositories({ "com.nookx.api.repository", "com.nookx.api.scraper.repository" })
+@EnableJpaRepositories({ "com.nookx.api.repository" })
 @EnableJpaAuditing(auditorAwareRef = "springSecurityAuditorAware")
 @EnableTransactionManagement
 public class DatabaseConfiguration {}
